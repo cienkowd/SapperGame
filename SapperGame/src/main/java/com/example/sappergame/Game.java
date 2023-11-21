5616a1e5527c0781e96a5d608838724b47d7d37b
cienkowd <01178827@pw.edu.pl>
2023-11-21 02:23:05 +0100

    new elements

--- SapperGame/src/main/java/com/example/sappergame/Game.java
+++ SapperGame/src/main/java/com/example/sappergame/Game.java
@@ -1,12 +1,8 @@
 package com.example.sappergame;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,7 +11,7 @@ public class Game extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 450, 450);
+        Scene scene = new Scene(fxmlLoader.load(), 630, 630);
         stage.setTitle("Sapper");
         stage.setScene(scene);
         stage.show();
--- SapperGame/src/main/java/com/example/sappergame/GameController.java
+++ SapperGame/src/main/java/com/example/sappergame/GameController.java
@@ -1,25 +1,24 @@
 package com.example.sappergame;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.io.UnsupportedEncodingException;
+import java.util.Objects;
+
 
 public class GameController extends Board{
 
     @FXML
     private GridPane gridPane;
-    @FXML
-    private Label gameOver;
     public void initialize() {
         Board board = new Board();
         board.generateBoard();
@@ -34,22 +33,96 @@ public class GameController extends Board{
 
     private Button createButton() {
         Button button = new Button();
-        button.setMinSize(50, 50);
-        button.setText("");
-        button.setOnAction(this::handleButtonClick);
+        button.setMinSize(70, 70);
+        button.setOnMouseClicked(this::handleButtonClick);
         return button;
     }
 
-    private void handleButtonClick(ActionEvent event) {
+    private void handleButtonClick(MouseEvent event) {
         Button clickedButton = (Button) event.getSource();
-        if(clickedButton.getId().equals("-1")) {
-            clickedButton.setStyle("-fx-background-color: #CC0000; -fx-text-fill: white;");
-            clickedButton.setText(clickedButton.getId());
-            showAlert("BOOOOMMMMM", "Game Over!");
+        if (event.getButton() == MouseButton.PRIMARY && whenYouCannotClickPrimary(clickedButton)) {
+            handleLeftClick(clickedButton);
+        }
+        else if (event.getButton() == MouseButton.SECONDARY && whenYouCannotClickOrWantCancelFlag(clickedButton)) {
+            handleRightClick(clickedButton);
+        }
+    }
+    private void handleLeftClick(Button button) {
+        if (button.getId().equals("0")) {
+            showAroundWhenZero(button);
         }
         else {
-            clickedButton.setText(clickedButton.getId());
+            setTextForButton(button);
         }
+    }
+
+    private void setTextForButton(Button button) {
+        if (button.getId().equals("-1")) {
+            button.setStyle("-fx-font-size: 30px; -fx-background-color: #CC0000; -fx-text-fill: white;");
+            button.setText(button.getId());
+            showAlert("BOOOOMMMMM", "Game Over!");
+
+        }
+        else if (button.getId().equals("1")) {
+            button.setText(button.getId());
+            button.setStyle("-fx-font-size: 32px; -fx-font-weight: bold; -fx-text-fill: blue;");
+        }
+        else if (button.getId().equals("2")) {
+            button.setText(button.getId());
+            button.setStyle("-fx-font-size: 32px; -fx-font-weight: bold; -fx-text-fill: green;");
+        }
+        else if (button.getId().equals("3")) {
+            button.setText(button.getId());
+            button.setStyle("-fx-font-size: 32px; -fx-font-weight: bold; -fx-text-fill: red;");
+        }
+        else if (button.getId().equals("4")) {
+            button.setText(button.getId());
+            button.setStyle("-fx-font-size: 32px; -fx-font-weight: bold; -fx-text-fill: #660099;");
+        }
+        else if (button.getId().equals("5")) {
+            button.setText(button.getId());
+            button.setStyle("-fx-font-size: 32px; -fx-font-weight: bold; -fx-text-fill: #660000;");
+        }
+        else if (button.getId().equals("6")) {
+            button.setText(button.getId());
+            button.setStyle("-fx-font-weight: bold; -fx-text-fill: #669966;");
+        }
+        else if (button.getId().equals("7")) {
+            button.setText(button.getId());
+            button.setStyle("-fx-font-weight: bold; -fx-text-fill: #006699;");
+        }
+        else
+            button.setText(button.getId());
+    }
+    private void handleRightClick(Button button) {
+        String imagePath = "/flaga.png";
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(imagePath)));
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(50);
+        imageView.setFitHeight(50);
+
+        button.setGraphic(imageView);
+    }
+
+    private boolean whenYouCannotClickPrimary(Button button) {
+        if(button.getGraphic() != null) {
+            return false;
+        }
+        else if(!(button.getText().isEmpty())) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean whenYouCannotClickOrWantCancelFlag(Button button) {
+        if(button.getGraphic() != null) {
+            button.setGraphic(null);
+            return false;
+        }
+        else if(!(button.getText().isEmpty())) {
+            return false;
+        }
+        return true;
     }
 
     private void showAlert(String title, String content) {
@@ -62,5 +135,28 @@ public class GameController extends Board{
         alertStage.initModality(Modality.APPLICATION_MODAL);
 
         alert.showAndWait();
+    }
+
+    private void showAroundWhenZero(Button button) {
+        int x = GridPane.getRowIndex(button);
+        int y = GridPane.getColumnIndex(button);
+        for(int  i = x-1; i <= x+1; i++) {
+            if(i >= 0 && i < 9)
+                for (int j = y-1; j <= y+1; j++)
+                    if (j >= 0 && j < 9) {
+                        Node node  = getNodeByRowColumnIndex(gridPane,i,j);
+                        if(node != null)
+                            setTextForButton((Button) node);
+                    }
+        }
+    }
+
+    private Node getNodeByRowColumnIndex(GridPane gridPane, int row, int col) {
+        for (Node node : gridPane.getChildren()) {
+            if (GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == col) {
+                return node;
+            }
+        }
+        return null;
     }
 }
